3de58156d982598ff987cd1bba81ba00b06a6319
miesnerjac <113484158+miesnerjac@users.noreply.github.com>
2023-02-16 04:05:13 -0600

    Add files via upload

---
+++ Models/Playlist.cs
@@ -0,0 +1,11 @@
+﻿namespace Music_Streaming_database.Models
+{
+    public class PlaylistsModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int userID { get; set; }
+    }
+}
---
+++ Models/song.cs
@@ -0,0 +1,17 @@
+﻿namespace Music_Streaming_database.Models
+{
+    public class songsModel
+    {
+        public int ID { get; set; }
+
+        public string name { get; set; }
+
+        public string relDate { get; set; }
+
+        public int length { get; set; }
+
+        public string genre { get; set; }
+
+        public string artName { get; set; }
+    }
+}
